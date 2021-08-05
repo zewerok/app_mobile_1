2800b306b256a33fe9e3d22443ef6b193eb1b08b
zewerok <zewerok@gmail.com>
2021-08-05 21:44:26 +0400

    App #1

---
+++ venv/main.py
@@ -0,0 +1,10 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text="Копытины, привет", halign="center")
+
+
+CopitiniApp().run()
